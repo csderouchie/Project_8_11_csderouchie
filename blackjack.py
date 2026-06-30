4fb9cfe97858b01f99a10aafefb5f02235ebfc6d
csderouchie <csderouchie@gmail.com>
2026-06-30 15:50:14 -0400

    Cleaned up code and documentation

--- blackjack.py
+++ blackjack.py
@@ -7,18 +7,19 @@ dealerHand = []
 chips = 100
 bet = 0
 
-#Creates and shuffles the deck
 def createDeck():
+    """Creates and shuffles the deck"""
     global deck
     global cards
+    deck = []
     i = 0
     while i < 4:
         deck += cards
         i+=1
     random.shuffle(deck)
 
-#Deals the passed number of cards to the passed hand
 def dealCards(hand, number):
+    """Deals the passed number of cards to the passed hand"""
     global deck
     i = 0
     while i < number:
@@ -26,8 +27,8 @@ def dealCards(hand, number):
         deck.pop()
         i+=1
 
-#Calculates the value of the passed hand
 def calculateValue(hand):
+    """Calculates the value of the passed hand"""
     value = 0
     aces = 0
     #Sum value of cards in hand, initially assuming aces = 11
@@ -46,12 +47,12 @@ def calculateValue(hand):
             aces -= 1
     return value
 
-#Adds a card to the player hand then check if they busted
 def hit(hand):
+    """Adds a card to the player hand then check if they busted"""
     global deck
     dealCards(hand, 1)
+    print(f"You got a {playerHand[-1]} making your total hand value {calculateValue(hand)}")
     if calculateValue(hand) > 21:
-        print(f"You got a {playerHand[-1]} making your total hand value {calculateValue(hand)}")
         lose()
 
 def stay(hand): #TODO: add dealer logic
@@ -67,44 +68,65 @@ def stay(hand): #TODO: add dealer logic
     else:
         split()
 
-def split():
+def splitPot():
+        """Resolves a tie in a round"""
         global bet
         global chips
         print(f"Pot split, you get your {bet} chip bet back")
         chips = chips + bet
         bet = 0
 
-#Resolves losing a round
 def lose():
+    """Resolves losing a round"""
     print("Round lost \n------------------------------------------------------------------------------------------")
     global bet
     bet = 0
 
-#Resolves winning a round
 def win():
+        """Resolves winning a round"""
         global bet
         global chips
         print(f"You won {bet * 2} chips! \n------------------------------------------------------------------------------------------")
         chips = chips + (bet * 2)
         bet = 0
 
-createDeck()
-#Game loop
+def newRound():
+    """Shuffles deck if needed then deals cards and calls placeBet() at the start of a round"""
+    global playerHand
+    global dealerHand
+    if len(deck) < 10:
+        createDeck()
+        print("Shuffling deck...")
+    dealCards(playerHand, 2)
+    dealCards(dealerHand, 2)
+    placeBet()
+
+def placeBet():
+    """Places bet for current round"""
+    global bet
+    global chips
+    while bet == 0:
+        bet = input(f"Current chips: {chips}\nPlace your bet: ")
+        try:
+            bet = int(bet)
+        except:
+            print("Invalid input. Must enter a whole number.")
+            bet = 0
+        else:
+            if bet > chips:
+                print("You cannot bet more chips than you have")
+                bet = 0
+            elif bet < 1:
+                print("You have to bet at least one chip")
+                bet = 0
+            else:
+                chips = chips - bet
+
+def main():
+    """Game loop"""
 while True:
     if bet == 0:
-        dealCards(playerHand, 2)
-        dealCards(dealerHand, 2)
-    while bet == 0:
-         bet = input(f"Current chips: {chips}\nPlace your bet: ")
-         bet = int(bet)
-         if bet > chips:
-             print("You cannot bet more chips than you have")
-             bet = 0
-         elif bet < 1:
-            print("You have to bet at least one chip")
-            bet = 0
-         else:
-             chips = chips - bet
+        newRound()
     print(f"Your cards: {playerHand} Current Value: {calculateValue(playerHand)}")
     print(f"Dealer showing: {dealerHand[0]}")
     userInput = input("| Hit | Stay | Double Down |\n")
@@ -114,3 +136,6 @@ while True:
         stay(playerHand)
     else:
         break
+
+if __name__ == "__main__":
+    main()
