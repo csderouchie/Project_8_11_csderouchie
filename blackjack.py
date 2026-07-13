e139e2aa093ed464da2d97a26aadcdff2e5c3a0f
csderouchie <csderouchie@gmail.com>
2026-07-13 05:58:38 -0400

    Refactored functions to work with game class

--- blackjack.py
+++ blackjack.py
@@ -9,157 +9,21 @@ import random
 import card
 import game
 
-def createDeck():
-    """Creates and shuffles a deck of card objects"""
-    names = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
-    deck = []
-    for x in range(4):
-        for name in names:
-            if type(name) == int:
-                deck.append(card.card(name, name))
-            elif name == "A":
-                deck.append(card.card(name, 11))
-            else:
-                deck.append(card.card(name, 10))
-    return random.shuffle(deck)
-
-def dealCards(hand, number):
-    """Deals the passed number of cards to the passed hand"""
-    i = 0
-    while i < number:
-        hand.append(deck[-1])
-        deck.pop()
-        i+=1
-
 def calculateValue(hand):
     """Calculates the value of the passed hand"""
     value = 0
     aces = 0
     #Sum value of cards in hand, initially assuming aces = 11
     for card in hand:
-        if type(card) == int:
-            value += card
-        elif card == "A":
-            value += 11
+        value += card.value
+        if card.name == "A":
             aces += 1
-        else:
-            value += 10
     ##Adjusts aces to equal 1 if hand value is greater than 21
     if value > 21 and aces > 0:
         while value > 21 and aces > 0:
             value -= 10
             aces -= 1
     return value
-
-def hit(hand):
-    """Adds a card to the player hand then check if they busted"""
-    global deck
-    dealCards(hand, 1)
-    print(f"You got a {playerHand[-1]} making your total hand value {calculateValue(hand)}")
-    if calculateValue(hand) > 21:
-        lose()
-
-def stay(hand):
-    """Reveals dealer hand then the dealer hits until their hand value is greater than 17. If the dealer did not bust hands are compared"""
-    global dealerHand
-    print(f"The dealer reveals a {dealerHand[-1]} making their current value {calculateValue(dealerHand)}")
-    if calculateValue(dealerHand) < 17:
-        dealerHit()
-    if bet > 0:
-        compareHands(hand)
-
-def compareHands(hand):
-    """Compares player and dealer hands to resolve a round"""
-    global dealerHand
-    dealer = calculateValue(dealerHand)
-    player = calculateValue(hand)
-    if player > dealer:
-        win()
-    elif player < dealer:
-        lose()
-    else:
-        splitPot()
-
-def doubleDown(hand):
-    """Doubles player bet, hits once, then stays"""
-    global bet
-    global chips
-    print(f"Putting another {bet} chips in for a total bet of {bet * 2}")
-    chips = chips - bet
-    bet = bet * 2
-    hit(hand)
-    if bet > 0:
-        stay(hand)
-
-def dealerHit():
-    """Adds cards to the dealer's hand until it's value is greater than 16. If their hand value exceeds 21 they bust and the player wins"""
-    global dealerHand
-    while calculateValue(dealerHand) < 17:
-        dealCards(dealerHand, 1)
-        print(f"The dealer hits revealing a {dealerHand[-1]} making their current hand value {calculateValue(dealerHand)}")
-        if calculateValue(dealerHand) > 21:
-            print("The dealer busted...")
-            win()
-    
-def splitPot():
-        """Resolves a tie in a round"""
-        global bet
-        global chips
-        print(f"Pot split, you get your {bet} chip bet back")
-        chips = chips + bet
-        bet = 0
-
-def lose():
-    """Resolves losing a round"""
-    print("Round lost \n------------------------------------------------------------------------------------------")
-    global bet
-    bet = 0
-
-def win():
-        """Resolves winning a round"""
-        global bet
-        global chips
-        print(f"You won {bet * 2} chips! \n------------------------------------------------------------------------------------------")
-        chips = chips + (bet * 2)
-        bet = 0
-
-def newRound():
-    """Shuffles deck if needed then deals cards and calls placeBet() at the start of a round"""
-    global playerHand
-    global dealerHand
-    global rounds
-    global deck
-    rounds += 1
-    if len(deck) < 10:
-        deck = createDeck()
-        print("Shuffling deck...")
-        print(deck)
-    playerHand = []
-    dealerHand = []
-    dealCards(playerHand, 2)
-    dealCards(dealerHand, 2)
-    placeBet()
-
-def placeBet():
-    """Places bet for current round"""
-    global bet
-    global chips
-    while bet == 0:
-        bet = input(f"Current chips: {chips}\nPlace your bet: ")
-        try:
-            bet = int(bet)
-        except:
-            print("Invalid input. Must enter a whole number.")
-            bet = 0
-        else:
-            if bet > chips:
-                print("You cannot bet more chips than you have")
-                bet = 0
-            elif bet < 1:
-                print("You have to bet at least one chip")
-                bet = 0
-            else:
-                chips = chips - bet
 
 def gameStats():
     """Prints stats at the end of the game"""
@@ -168,44 +32,45 @@ def gameStats():
     print(f"\n------------------------------------------------------------------------------------------\nGame Over Summary\nRounds played: {rounds}\nEnding Chips: {chips}\nNet chips per round: {(chips - 100) / rounds}")
 
 def loadGame():
-    global game
-    userInput = input(f"Blackjack\n1. New Game\n2. Load Game\nChoose option: ")
-    if userInput == "1":
-        return game.game(createDeck(), 100)
-    elif userInput == "2":
-        print("TODO: Add load game functionality")
-    else:
-        print("Invalid input")
+    while True:
+        userInput = input(f"Blackjack\n1. New Game\n2. Load Game\nChoose option: ")
+        if userInput == "1":
+            newGame = game.game()
+            return newGame
+        elif userInput == "2":
+            print("TODO: Add load game functionality")
+        else:
+            print("Invalid input")
 
 def main(game):
     """Game loop"""
     while True:
         if game.bet == 0:
-            newRound()
-        print(f"Your cards: {playerHand} Current Value: {calculateValue(playerHand)}")
-        print(f"Dealer showing: {dealerHand[0]}")
-        if len(playerHand) == 2:
+            game = game.newRound()
+        print(f"Your cards: {game.playerHand} Current Value: {calculateValue(game.playerHand)}")
+        print(f"Dealer showing: {game.dealerHand[0]}")
+        if len(game.playerHand) == 2:
             userInput = input(f"| Hit | Stay | Double Down |\n")
         else:
             userInput = input(f"| Hit | Stay |\n")
         if userInput.lower() == "hit" or userInput.lower() == "h":
-            hit(playerHand)
+            game.hit()
         elif userInput.lower() == "stay" or userInput.lower() == "s":
-            stay(playerHand)
+            game.stay()
         elif userInput.lower() == "double down" or userInput.lower() == "d":
-            if len(playerHand) > 2:
+            if len(game.playerHand) > 2:
                 print("You can only double down on the first action of a round")
-            elif bet > chips:
+            elif game.bet > game.chips:
                 print("You do not have enough chips to double down")
             else:
-                doubleDown(playerHand)
+                game.doubleDown()
         else:
             print("Invalid input. Choose to 'stay', 'hit', or 'double down'")
             #Exit conditions
-        if chips == 0 and bet == 0:
+        if game.chips == 0 and game.bet == 0:
             gameStats()
             break
-        if bet == 0:
+        if game.bet == 0:
             userInput = input("Press enter to play again or press 'q' to quit ")
             if userInput.lower() == "q" or userInput.lower() == "quit":
                 gameStats()
--- game.py
+++ game.py
@@ -4,11 +4,129 @@ Cedric DeRouchie
 Terminal app for playing the game blackjack
 7/13/2026
 """
+import card
+import blackjack
 
 class game:
-    def __init__(self, deck, chips):
-        self.deck = deck
+    def __init__(self):
+        self.deck = []
         self.playerHand = []
         self.dealerHand = []
-        self.chips = chips
+        self.chips = 100
         self.bet = 0
+
+    def dealCards(self, hand, number):
+        """Deals the passed number of cards to the passed hand"""
+        i = 0
+        while i < number:
+            hand.append(self.deck[-1])
+            self.deck.pop()
+            i+=1
+        return self
+
+    def createDeck(self):
+        """Creates and shuffles a deck of card objects"""
+        names = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
+        deck = []
+        for x in range(4):
+            for name in names:
+                if type(name) == int:
+                    deck.append(card.card(name, name))
+                elif name == "A":
+                    deck.append(card.card(name, 11))
+                else:
+                    deck.append(card.card(name, 10))
+        self.deck = deck
+        return self
+    
+    def hit(self):
+        """Adds a card to the player hand then check if they busted TODO fix"""
+        self.dealCards(self.playerHand, 1)
+        print(f"You got a {self.playerHand[-1]} making your total hand value {blackjack.calculateValue(self.playerHand)}")
+        if blackjack.calculateValue(self.playerHand) > 21:
+            self.lose()
+
+    def splitPot(self):
+        """Resolves a tie in a round"""
+        print(f"Pot split, you get your {self.bet} chip bet back")
+        self.chips = self.chips + self.bet
+        self.bet = 0
+        return self
+
+    def lose(self):
+        """Resolves losing a round"""
+        print("Round lost \n------------------------------------------------------------------------------------------")
+        self.bet = 0
+        return self
+
+    def win(self):
+            """Resolves winning a round"""
+            print(f"You won {self.bet * 2} chips! \n------------------------------------------------------------------------------------------")
+            self.chips = self.chips + (self.bet * 2)
+            self.bet = 0
+            return self
+
+    def compareHands(self):
+        """Compares player and dealer hands to resolve a round"""
+        if self.playerHand > self.dealerHand:
+            self.win()
+        elif self.playerHand < self.dealerHand:
+            self.lose()
+        else:
+            self.splitPot()
+
+    def doubleDown(self):
+        """Doubles player bet, hits once, then stays"""
+        print(f"Putting another {self.bet} chips in for a total bet of {self.bet * 2}")
+        self.chips = self.chips - self.bet
+        self.bet = self.bet * 2
+        self.hit()
+        if self.bet > 0:
+            self.stay()
+
+    def stay(self):
+        """Reveals dealer hand then the dealer hits until their hand value is greater than 17. If the dealer did not bust hands are compared"""
+        print(f"The dealer reveals a {self.dealerHand[-1]} making their current value {blackjack.calculateValue(self.dealerHand)}")
+        if blackjack.calculateValue(self.dealerHanddealerHand) < 17:
+            self.dealerHit()
+        if self.bet > 0:
+            self.compareHands()
+
+    def dealerHit(self):
+        """Adds cards to the dealer's hand until it's value is greater than 16. If their hand value exceeds 21 they bust and the player wins"""
+        while blackjack.calculateValue(self.dealerHand) < 17:
+            self.dealCards(self.dealerHand, 1)
+            print(f"The dealer hits revealing a {self.dealerHand[-1]} making their current hand value {blackjack.calculateValue(self.dealerHand)}")
+            if blackjack.calculateValue(self.dealerHand) > 21:
+                print("The dealer busted...")
+                self.win()
+
+    def placeBet(self):
+        """Places bet for current round"""
+        while self.bet == 0:
+            bet = input(f"Current chips: {self.chips}\nPlace your bet: ")
+            try:
+                bet = int(bet)
+            except:
+                print("Invalid input. Must enter a whole number.")
+                self.bet = 0
+            else:
+                if self.bet > self.chips:
+                    print("You cannot bet more chips than you have")
+                    self.bet = 0
+                elif self.bet < 1:
+                    print("You have to bet at least one chip")
+                    self.bet = 0
+                else:
+                    self.chips = self.chips - self.bet
+
+    def newRound(self):
+        """Shuffles deck if needed then deals cards and calls placeBet() at the start of a round"""
+        if len(self.deck) < 10:
+            self.createDeck()
+            #print("Shuffling deck...")
+        self.playerHand = []
+        self.dealerHand = []
+        self.dealCards(self.playerHand, 2)
+        self.dealCards(self.dealerHand, 2)
+        return self
